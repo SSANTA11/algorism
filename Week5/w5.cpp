df79e54a14787f2b79594e0ae47b89be00e48e31
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-10-24 02:11:41 +0900

    수정

--- Week5/w5.cpp
+++ Week5/w5.cpp
@@ -25,7 +25,7 @@
 #define MAX_VERTICES 10	// 그래프의 정점 개수
 
 int distance[MAX_VERTICES];	// 시작 정점으로부터의 최단 경로 길이 저장
-int S[MAX_VERTICES];				// 정점의 집합 S
+int S[MAX_VERTICES];		// 정점의 집합 S
 
 int nextVertex(int n);
 int printStep(int step);
